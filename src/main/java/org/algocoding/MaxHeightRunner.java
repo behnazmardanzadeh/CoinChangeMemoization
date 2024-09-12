d2ed454d5381acbbda2193da72a588b667dc0946
Behnaz Mardanzadeh <b.mardanzadeh@charisma.tech>
2024-09-12 18:19:54 +0330

    Coin Change Memoization

--- src/main/java/org/algocoding/MaxHeightRunner.java
+++ src/main/java/org/algocoding/MaxHeightRunner.java
@@ -6,42 +6,68 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public class Main {
-    private final static int[] coins = new int[]{1, 5, 10, 25, 50};
-    private static long[][] memoization = new long[2000000][5];
-
+public class MaxHeightRunner {
+    private final static int[] coins = new int[]{2, 3, 5};
+    private static long[][] memoization = new long[2000000][3];
     private static int cnt;
+    private static int maxHeight;
 
     public static void main(String[] args) {
         Arrays.stream(memoization).forEach(amounts -> Arrays.fill(amounts, -1));
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))){
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))) {
 
             int amount = Integer.parseInt(reader.readLine());
-            long start = System.currentTimeMillis() * 1000;
             cnt = 0;
-            long totalCombinations = coinChange(amount, 0);
-            long end = System.currentTimeMillis() * 1000;
-            long duration = end - start;
-            System.out.println(totalCombinations);
-            System.out.println(String.format("duration: %,d", duration));
-            System.out.println("Recursion Counter: " + cnt);
+            maxHeight = 0;
+            long totalCombinations = coinChange(amount, 0, 0);
+            System.out.println(String.format("\nTotal Combinations: %,d\n", totalCombinations));
+            System.out.println(String.format("Recursion Counter: %,d\n", cnt));
+            System.out.println("Max Height: " + maxHeight);
+
+            System.out.print("\n    ");
+            for (int i = 0; i < 3; i++){
+                System.out.print(coins[i] + "  ");
+            }
+            System.out.println();
+            for(int i = 0; i <= amount; i++) {
+                System.out.print(i + "  ");
+                for (int j = 0; j < 3; j++) {
+                    if (memoization[i][j] >= 0 && memoization[i][j] < 10) {
+                        System.out.print(" ");
+                    }
+                    System.out.print(memoization[i][j] + " ");
+                }
+                System.out.println();
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    private static long coinChange(int amount, int coinIndex) {
+
+    private static long coinChange(int amount, int coinIndex, int height) {
         cnt++;
         if (amount == 0) {
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
             return 1;
         }
         if (amount < 0 || coinIndex >= coins.length) {
+            if (height > maxHeight) {
+                maxHeight = height;
+            }
             return 0;
         }
         if (memoization[amount][coinIndex] != -1) {
+            System.out.println("Yes! memoization[" + amount + "][" + coins[coinIndex] + "] = " + memoization[amount][coinIndex]);
             return memoization[amount][coinIndex];
         }
-        return memoization[amount][coinIndex] = coinChange(amount - coins[coinIndex], coinIndex)
-                + coinChange(amount, coinIndex + 1);
+
+        memoization[amount][coinIndex] = coinChange(amount - coins[coinIndex], coinIndex, height + 1)
+                + coinChange(amount, coinIndex + 1, height + 1);
+
+        //System.out.println("memoization[" + amount + "][" + coins[coinIndex] + "] = " + memoization[amount][coinIndex]);
+        return memoization[amount][coinIndex];
     }
 }
--- src/main/java/org/algocoding/Runner.java
+++ src/main/java/org/algocoding/Runner.java
@@ -6,32 +6,23 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public class Main {
-    private final static int[] coins = new int[]{1, 5, 10, 25, 50};
-    private static long[][] memoization = new long[2000000][5];
-
-    private static int cnt;
+public class Runner {
+    private final static int[] coins = new int[]{2, 3, 5};
+    private static long[][] memoization = new long[2000000][3];
 
     public static void main(String[] args) {
         Arrays.stream(memoization).forEach(amounts -> Arrays.fill(amounts, -1));
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))){
 
             int amount = Integer.parseInt(reader.readLine());
-            long start = System.currentTimeMillis() * 1000;
-            cnt = 0;
             long totalCombinations = coinChange(amount, 0);
-            long end = System.currentTimeMillis() * 1000;
-            long duration = end - start;
-            System.out.println(totalCombinations);
-            System.out.println(String.format("duration: %,d", duration));
-            System.out.println("Recursion Counter: " + cnt);
+            System.out.println(String.format("Total Combinations: %,d\n", totalCombinations));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     private static long coinChange(int amount, int coinIndex) {
-        cnt++;
         if (amount == 0) {
             return 1;
         }
